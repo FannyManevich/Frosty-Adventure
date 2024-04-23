b69a5f6ed5a5aa21383cf9baa363cf5a91afd3bb
FannyManevich <fmanevich@gmail.com>
2024-04-23 20:48:44 +0300

    Update
    Updating the grid

--- Frosty-Adventure/Assets/Scripts/Player/PlayerMovement.cs
+++ Frosty-Adventure/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,28 +1,41 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Transform leftWall;
+    public Transform rightWall;
+    public Transform bottomWall;
+
     private Rigidbody2D rb;
     private float moveSpeed = 100.0f;
     private Vector2 moveDirection = Vector2.zero;
-    public Transform bottomWall;
+
+    
 
     InputChannel inputChannel;
-
 
     void Start()
     {
         AddListeners();
-    }
+    }    
 
-    
+
     void Update()
     {
-      
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 newPosition = transform.position + new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, leftWall.position.x + 0.5f, rightWall.position.x - 0.5f);
+        newPosition.y = Mathf.Clamp(newPosition.y, bottomWall.position.y + 0.5f, Mathf.Infinity);
+
+        transform.position = newPosition;
     }
+
 
     private void FixedUpdate()
     { 
