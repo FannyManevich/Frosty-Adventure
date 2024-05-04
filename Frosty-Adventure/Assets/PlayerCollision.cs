ca80d1f08031c729c2e8b194143836def15fb149
FannyManevich <fmanevich@gmail.com>
2024-05-04 22:26:23 +0300

    New Input System
     fixed

---
+++ Frosty-Adventure/Assets/PlayerCollision.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    //private void OnCollisionEnter2D(Collision2D collision)
+   // {
+       // if (collision.transform.tag == "Enemy")
+       // {
+         //   HealthManager.health--;
+           // if (HealthManager.health <= 0)
+          //  {
+               // PlayerManager.isGameOver = true;
+                //AudioManager.instance.Play("GameOver");
+                //gameObject.SetActive(false);
+           // }
+          //  else
+           // {
+               //StartCoroutine(GetHurt());
+           // }
+       // }
+   // }
+}
