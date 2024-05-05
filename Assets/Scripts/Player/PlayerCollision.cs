b5acf3794f9a10613369428093e481e115590855
AmitSherman1 <104097658+AmitSherman1@users.noreply.github.com>
2024-05-05 23:49:00 +0300

    HealthBar

---
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    //private void OnCollisionEnter2D(Collision2D collision)
+    // {
+    // if (collision.transform.tag == "Enemy")
+    // {
+    //   HealthManager.health--;
+    // if (HealthManager.health <= 0)
+    //  {
+    // PlayerManager.isGameOver = true;
+    //AudioManager.instance.Play("GameOver");
+    //gameObject.SetActive(false);
+    // }
+    //  else
+    // {
+    //StartCoroutine(GetHurt());
+    // }
+    // }
+    // }
+
+
+}
