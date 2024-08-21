e8553272827d3a959fcbe9a224030c8d56a9f0e2
FannyManevich <fmanevich@gmail.com>
2024-08-21 12:01:26 +0300

    Game state
    game state update

---
+++ Frosty-Adventure/Assets/Scripts/Managers/GameStateManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour
+{
+  //  public List<GameState> states;
+  //  public List<TransitionBase> transitions;
+   // public GameState currentGameState;
+  //  public GameState defaultGameState;
+  //  public GameStateChannel gameStateChannel;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Frosty-Adventure/Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            if(instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+       // DontDestroy(gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+}
