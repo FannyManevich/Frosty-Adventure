6cf0dc808c843c5944a4bd6ebc645ea5be9d74c4
FannyManevich <fmanevich@gmail.com>
2024-09-16 16:43:02 +0300

    water collector

--- Frosty-Adventure/Assets/Scripts/Player/PlayerCollisionDetection.cs
+++ Frosty-Adventure/Assets/Scripts/Player/PlayerCollisionDetection.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CollisionDetection : MonoBehaviour
+public class PlayerCollisionDetection : MonoBehaviour
 {
     public Text WaterText;
     private int countBottles;
-    public Transform leftWall;
-    public Transform rightWall;
-    public Transform bottomWall;
-    public Transform topWall;
 
     public void Start()
     {
@@ -22,21 +18,21 @@ public class CollisionDetection : MonoBehaviour
     {
         if (other.CompareTag("Collectible"))
         {
+
+            Destroy(other.gameObject);
+
             Debug.Log("Water bottle collected!");
             countBottles += 1;
 
             UpdateThirstText();
-
-            Destroy(other.gameObject);
         }
     }
 
     private void UpdateThirstText()
     {
-        Debug.Log("Updating water counter: " + countBottles);
         if (WaterText != null)
-        {
-            WaterText.text = "Water collected : " + countBottles;
+        {       
+            WaterText.text = "Water collected: " + countBottles;
         }
         else
         {
--- Frosty-Adventure/Assets/Scripts/SceneLoader.cs
+++ Frosty-Adventure/Assets/Scripts/SceneLoader.cs
@@ -1,21 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;  // Required for interacting with UI buttons
+using UnityEngine.UI;  
 
 public class SceneLoader : MonoBehaviour
 {
     public string sceneNameToLoad;
     public KeyCode activationKey = KeyCode.UpArrow;  
-    public Button myButton; // Reference to the UI Button
+    public Button myButton;
 
     private bool isPlayerOnTile = false;
 
     void Start()
     {
-        // Optionally, add a button listener if the button is assigned
+        
         if (myButton != null)
         {
-            myButton.onClick.AddListener(LoadScene);  // Use LoadScene as the method to call when the button is clicked
+            myButton.onClick.AddListener(LoadScene);  
         }
     }
 
@@ -37,7 +37,7 @@ public class SceneLoader : MonoBehaviour
 
     void Update()
     {
-        // Check if the player is on the tile and the specific key is pressed
+        
         if (isPlayerOnTile && Input.GetKeyDown(activationKey))
         {
             LoadScene();
